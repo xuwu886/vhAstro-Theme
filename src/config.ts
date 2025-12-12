c804efca387e18cf90eb6cc0fd6970865aecf8b8
xuwu886 <xuwu886@gmail.com>
2025-12-13 00:40:12 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,113 @@
-加载中...
+export default {
+  Title: 'XUWU的blog',
+  Site: 'https://www.vvhan.com',
+  Subtitle: '很好看的博客',
+  Description: ' 嘿嘿嘿',
+  Author: '作者昵称',
+  Motto: '晚风吹过～',
+  Avatar: 'https://tg.xuwujk.com/file/1765557548808_1000017364.jpg',
+  // Cover 网站缩略图
+  Cover: '/assets/images/banner/072c12ec85d2d3b5.webp',
+  // 网站创建时间
+  CreateTime: '2025-09-01',
+  // 首页打字机文案列表
+  TypeWriteList: [
+    '首页打字机的第一句',
+    "首页打字机的第二句，可以继续往后加",
+  ],
+  // 顶部 Banner 配置
+  HomeBanner: {
+    enable: true,
+    cover: '/assets/images/home-banner.webp'
+  },
+  // 博客主题配置
+  Theme: {
+    // 颜色请用 16 进制颜色码
+    // 主题颜色
+    "--vh-main-color": "#01C4B6",
+    // 字体颜色
+    "--vh-font-color": "#34495e",
+    // 侧边栏宽度
+    "--vh-aside-width": "318px",
+    // 全局圆角
+    "--vh-main-radius": "0.88rem",
+    // 主体内容宽度
+    "--vh-main-max-width": "1458px",
+  },
+  // 导航栏 (新窗口打开 newWindow: true)
+  Navs: [
+    // 仅支持 SVG 且 SVG 需放在 public/assets/images/svg/ 目录下，填入文件名即可（封装了 SVG 组件 为了极致压缩 SVG）
+    // 建议使用 https://tabler.io/icons 直接下载 SVG
+    { text: '朋友', link: '/links', icon: 'Nav_friends' },
+    { text: '圈子', link: '/friends', icon: 'Nav_rss' },
+    { text: '动态', link: '/talking', icon: 'Nav_talking' },
+    { text: '昔日', link: '/archives', icon: 'Nav_archives' },
+    { text: '留言', link: '/message', icon: 'Nav_message' },
+    { text: '关于', link: '/about', icon: 'Nav_about' },
+    { text: 'API', link: 'https://api.vvhan.com/', target: true, icon: 'Nav_link' },
+  ],
+  // 侧边栏个人网站
+  WebSites: [
+    // 仅支持 SVG 且 SVG 需放在 public/assets/images/svg/ 目录下，填入文件名即可（封装了 SVG 组件 为了极致压缩 SVG）
+    // 建议使用 https://tabler.io/icons 直接下载 SVG
+    // 这里的按照他的格式可以改或者添、删
+    { text: 'Github', link: 'https://github.com/uxiaohan', icon: 'WebSite_github' },
+    { text: '韩小韩API', link: 'https://api.vvhan.com', icon: 'WebSite_api' },
+    { text: '每日热榜', link: 'https://hot.vvhan.com', icon: 'WebSite_hot' },
+    { text: '骤雨重山图床', link: 'https://wp-cdn.4ce.cn', icon: 'WebSite_img' },
+    { text: 'HanAnalytics', link: 'https://analytics.vvhan.com', icon: 'WebSite_analytics' },
+  ],
+  // 侧边栏展示
+  AsideShow: {
+    // 是否展示个人网站
+    WebSitesShow: true,
+    // 是否展示分类
+    CategoriesShow: true,
+    // 是否展示个人标签
+    TagsShow: true,
+    // 是否展示推荐文章
+    recommendArticleShow: true
+  },
+  // DNS预解析地址
+  DNSOptimization: [
+    'https://i0.wp.com',
+    'https://analytics.vvhan.com',
+    'https://vh-api.4ce.cn',
+    'https://registry.npmmirror.com',
+    'https://pagead2.googlesyndication.com'
+  ],
+  // 博客音乐组件解析接口
+  vhMusicApi: 'https://vh-api.4ce.cn/blog/meting',
+  // 评论组件（只允许同时开启一个）
+  Comment: {
+    // Twikoo 评论
+    Twikoo: {
+      enable: false,
+      envId: ''
+    },
+    // Waline 评论
+    Waline: {
+      enable: false,
+      serverURL: ''
+    }
+  },
+  // Han Analytics 统计（https://github.com/uxiaohan/HanAnalytics）
+  HanAnalytics: { enable: true, server: 'https://analytics.vvhan.com', siteId: 'Hello-HanHexoBlog' },
+  // Google 广告
+  // 一般不用管
+  GoogleAds: {
+    ad_Client: 'ca-pub-xxxxxxxxxx',
+    // 侧边栏广告(不填不开启)
+    asideAD_Slot: `<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-xxxxxxxxxx" data-ad-slot="xxxxxx" data-ad-format="auto" data-full-width-responsive="true"></ins>`,
+    // 文章页广告(不填不开启)
+    articleAD_Slot: `<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-xxxxxxxxxx" data-ad-slot="xxxxxx" data-ad-format="auto" data-full-width-responsive="true"></ins>`
+  },
+  // 文章内赞赏码
+  // 自己去仓库内下面这两个地址将图片换成自己的收款码
+  Reward: {
+    // 支付宝收款码
+    AliPay: '/assets/images/alipay.webp',
+    // 微信收款码
+    WeChat: '/assets/images/wechat.webp'
+  }
+}
